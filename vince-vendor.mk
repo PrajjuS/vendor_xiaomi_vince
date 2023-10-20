34d645d784a7e99c351e57a72aed59740498f82c
Leonardo Ledda <leonardoledda@gmail.com>
2023-10-20 15:59:12 +0530

    vince: Add prebuilt libprotobuf-cpp-lite-3.9.1 from redfin
    - Sensors and RIL need it
    
    Fixes:
      10-20 03:17:59.056  5255  5255 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.drm@1.3-service.widevine": library "libprotobuf-cpp-lite-3.9.1.so" not found: needed by /vendor/lib64/libwvhidl.so in namespace (default)
    
    Change-Id: Ibdc2c4d2c61720f82152e7c328b5357f9376206a
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- vince-vendor.mk
+++ vince-vendor.mk
@@ -950,6 +950,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/vince/proprietary/vendor/lib64/libperfconfig.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libperfconfig.so \
     vendor/xiaomi/vince/proprietary/vendor/lib64/libperfgluelayer.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libperfgluelayer.so \
     vendor/xiaomi/vince/proprietary/vendor/lib64/libperipheral_client.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libperipheral_client.so \
+    vendor/xiaomi/vince/proprietary/vendor/lib64/libprotobuf-cpp-lite-3.9.1.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-3.9.1.so \
     vendor/xiaomi/vince/proprietary/vendor/lib64/libpvr.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libpvr.so \
     vendor/xiaomi/vince/proprietary/vendor/lib64/libqcc_file_agent.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqcc_file_agent.so \
     vendor/xiaomi/vince/proprietary/vendor/lib64/libqcci_legacy.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqcci_legacy.so \
